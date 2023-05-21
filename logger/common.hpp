1a7780909b7e05b778aec6cf1ef0580df9a17044
SKaminski <47929278+Lt1Gt0@users.noreply.github.com>
2023-05-20 20:32:14 -0700

    Update common.hpp

--- logger/common.hpp
+++ logger/common.hpp
@@ -29,28 +29,5 @@ inline constexpr void error(Severity severity, T head, Ts... tail)
     std::cerr << head << " ";
     error(severity, tail...);
 }
-
-namespace Debug
-{
-    inline void Print(const char* fmt, ...)
-    {
-        va_list ap;
-        va_start(ap, fmt);
-        vfprintf(stdout, fmt, ap);
-        fprintf(stdout, "\n");
-        fflush(stdout);
-        va_end(ap);
-    }
-    
-    inline void PrintErr(const char* fmt, ...)
-    {
-        va_list ap;
-        va_start(ap, fmt);
-        vfprintf(stderr, fmt, ap);
-        fprintf(stderr, "\n");
-        fflush(stderr);
-        va_end(ap);
-    }
-}
-
+       
 #endif // _COMMON_HPP_
